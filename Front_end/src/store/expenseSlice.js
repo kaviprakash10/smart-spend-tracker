9adf66d2dac1814611c2315d71eb8e9b128c6210
kaviprakash <kaviprakash2383@gmail.com>
2026-06-10 14:31:14 +0530

    Update API_URL for expenseSlice

--- Front_end/src/store/expenseSlice.js
+++ Front_end/src/store/expenseSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "https://smart-spend-tracker-backend.onrender.com/api/expenses";
+const API_URL = "https://smart-spend-tracker-backend-fm2u.onrender.com/api/expenses";
 
 // Get all expenses
 export const getExpenses = createAsyncThunk(
