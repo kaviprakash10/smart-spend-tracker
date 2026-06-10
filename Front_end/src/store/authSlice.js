6625094d3e5c9edb1be385f80a34824688a53ca2
kaviprakash <kaviprakash2383@gmail.com>
2026-06-10 14:30:51 +0530

    Update API URL for authentication service

--- Front_end/src/store/authSlice.js
+++ Front_end/src/store/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "https://smart-spend-tracker-backend.onrender.com/api/auth";
+const API_URL = "https://smart-spend-tracker-backend-fm2u.onrender.com/api/auth";
 
 // User Registration
 export const registerUser = createAsyncThunk(
